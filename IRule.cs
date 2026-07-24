b192caff7ca2a41dd758ca2ce788e6e9b88a1d2d
Mohamed Khaled <amer31824@gmail.com>
2026-07-24 17:52:05 +0300

    IRule Interface

--- IRule.cs
+++ IRule.cs
@@ -2,10 +2,12 @@ using System.Collections.Generic;
 
 namespace QuantumRadar
 {
+    #region IRule Interface
     public interface IRule
     {
         string Name { get; }
 
         List<Violation> Evaluate(Observation observation);
-    }
+    } 
+    #endregion
 }
